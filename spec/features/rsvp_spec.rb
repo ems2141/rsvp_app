0e291f94c16ac2048ae2fbeaf11a7f3a70043f1b
Ellie Schneiders <ems2141@gmail.com>
2014-04-25 14:14:00 -0600

    Added 12factor gem and updated README

--- spec/features/rsvp_spec.rb
+++ spec/features/rsvp_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'it has a welcome page' do
   scenario 'user can visit welcome page' do
     visit '/'
-    save_and_open_page
     expect(page).to have_content "Welcome to Ellie and Nick's Wedding Website"
+    save_and_open_page
   end
 end
