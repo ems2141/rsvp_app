a6371cf6a6ada376398433c6da5737511cb11f3e
Ellie Schneiders <ems2141@gmail.com>
2014-05-29 08:48:13 -0600

    refactored to user form_for for login to persist login data for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       log_user_in(@user)
       flash[:login_message] = "Welcome back #{@user.email}"
       redirect_to '/welcome'
