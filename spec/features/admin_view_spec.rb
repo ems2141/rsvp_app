efc7836f0a0ba71d9f728cba50784709798b94cf
Ellie Schneiders <ems2141@gmail.com>
2014-08-18 20:48:36 -0600

    admin user can click link to return home from users index

--- spec/features/admin_view_spec.rb
+++ spec/features/admin_view_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 feature 'Admin' do
-  scenario 'admin users can view list of registered guests' do
-    create_user(admin: true)
+
+  before do
+    create_user(
+        email: 'jake@example.com',
+        password: 'hello123',
+        admin: true)
 
     create_user(name: 'Tommy',
-    email: 'tommy@test.com')
+                email: 'tommy@test.com')
 
     create_user(name: 'Meg',
-    email: 'meg@test.com')
+                email: 'meg@test.com')
+  end
 
+  scenario 'admin users can view list of registered guests' do
     visit '/login'
     fill_in 'Email', with: 'jake@example.com'
     fill_in 'Password', with: 'hello123'
@@ -18,5 +24,17 @@ feature 'Admin' do
     click_on 'View Guests'
     expect(page).to have_content 'Meg'
     expect(page).to have_content 'Tommy'
+  end
+
+  scenario 'user can click link to return to homepage from the users index' do
+    visit '/login'
+    fill_in 'Email', with: 'jake@example.com'
+    fill_in 'Password', with: 'hello123'
+    click_on 'Login'
+
+    click_on 'View Guests'
+    expect(page).to have_content 'Meg'
+    click_on 'Home'
+    expect(page).to have_content 'Welcome Jake!'
   end
 end
